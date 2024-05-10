37ca269c4ebb26cea70b80ce7e790aadd9ce0355
Kazi Omar Faruque <kazifdev@outlook.com>
2024-05-10 20:38:58 +1000

    updated portfolio

--- js/main.js
+++ js/main.js
@@ -11,28 +11,28 @@ const testimonials = [
   {
     name: "Lidija Bicker",
     position: "Training Consultant",
-    photo: "/img/lidija.jpg",
+    photo: "img/lidija.jpg",
     text:
       "I have mentored and coached Kazi through his IT Professional Year programme, focusing on business skills. I have found Kazi very professional and dedicated to his chosen area of expertise, always delivering high quality of reports, blogs, and presentations. His attitude is always positive towards his peers, workload and teamwork. I would strongly recommend Kazi to any suitable workplace as I believe he will always contribute to the best of his abilities."
   },
   {
     name: "MD Asif Iqbal",
     position: "Data Analyst @ Hydro Tasmania",
-    photo: "/img/asif.jpg",
+    photo: "img/asif.jpg",
     text:
       "I am pleased to recommend Kazi Faruque for his exceptional work as an IT intern at Travel With A Cause. Kazi was a valuable member of our team and contributed greatly to our IT department during his internship. Kazi's hard work, dedication, and positive attitude were evident throughout his time at our company."
   },
   {
     name: "Atiq Khan",
     position: "Technologist @ABC",
-    photo: "/img/atiq.jpg",
+    photo: "img/atiq.jpg",
     text:
       "I highly recommend working with Kazi on any future group projects as he consistently brings positive energy, strong leadership skills, and valuable insights to the team. His technical proficiency, professionalism, and dedication to growth are exceptional. Kazi quickly grasps complex concepts and applies them effectively."
   },
   {
     name: "Alex Afshar",
     position: "SAN Administrator @SeaRoad",
-    photo: "/img/alex.jpg",
+    photo: "img/alex.jpg",
     text:
       "Working with Kazi, I learned that how someone can give us great ideas to overcome challenges in our Project Management activities. I really liked working with him in a group specially because of his attention to the details and reminding us to put more attention on them. Kazi is also very professional in his Front End skills as he has been spending so much time to learn, practice and implement new skills!"
   }
